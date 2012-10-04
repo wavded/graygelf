4b5d37a6c08c0b9825a47e315422eba761039cc2
Marc Harter <wavded@gmail.com>
2012-10-04 10:19:35 -0500

    updated; fixed tests

--- test/server-test.js
+++ test/server-test.js
@@ -4,17 +4,17 @@ var graygelf = require('../')
 
 suite('general', function () {
   test('exports a single function', function () {
-    assert.equal(typeof graygelf.server, 'function')
+    assert.equal(typeof graygelf.createServer, 'function')
   })
 
   test('defaults to port 12201 and binds to address 0.0.0.0', function () {
-    var gg = graygelf.server()
+    var gg = graygelf.createServer().listen()
     assert.equal(gg.address, '0.0.0.0')
     assert.equal(gg.port, 12201)
   })
 
   test('accepts port and address', function () {
-    var gg = graygelf.server({ port: 32323, address: '127.0.0.1' })
+    var gg = graygelf.createServer().listen(32323, '127.0.0.1')
     assert.equal(gg.address, '127.0.0.1')
     assert.equal(gg.port, 32323)
   })
